0ac7a6af51264bb2575ea7526dcecdd5bc081607
xjh <413112417@qq.com>
2017-12-14 13:25:55 +0800

    update

---
+++ 22_头文件/有条件引用/header.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int test () {
+    printf("from header.h\n");
+    return 0;
+}
+
+#endif
---
+++ 22_头文件/有条件引用/hello.c
@@ -0,0 +1,9 @@
+
+#define SYSTEM_H "header.h"
+
+#include SYSTEM_H
+
+int main (void)
+{
+   test ();
+}
